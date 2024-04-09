69c3aa068f74c01c8453bdbb8fad9b6665ab863a
Anna Chernova <annanvks@gmail.com>
2024-04-09 12:34:19 +0100

    add basic cards styling

--- src/components/MemoryGame/index.jsx
+++ src/components/MemoryGame/index.jsx
@@ -1,4 +1,6 @@
-const cards = [1, 2, 3, 4, 5];
+import "./style.css";
+
+const cards = [1, 2, 3, 4, 5, 6];
 function shuffle(array) {
   console.log(array);
   for (let i = array.length - 1; i > 0; i--) {
@@ -9,7 +11,7 @@ function shuffle(array) {
   return array;
 }
 
-const allCards = shuffle(cards.concat(cards)); // make a doubled deck of
+const allCards = shuffle(cards.concat(cards)); // make a doubled deck of cards
 
 const handleCkick = () => {};
 function Game() {
@@ -18,9 +20,9 @@ function Game() {
       <div className="cardsContainer">
         {allCards.map((item, index) => {
           return (
-            <div key={index} className="cards" onClick={handleCkick}>
+            <div key={index} className="card" onClick={handleCkick}>
               <div className="cardBack"></div>
-              {item}
+              <div className="cardContent">{item}</div>
             </div>
           );
         })}
