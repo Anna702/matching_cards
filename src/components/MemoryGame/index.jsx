7f7121daaf3f4ce3b3697d21d41e71f4d9045624
Anna Chernova <annanvks@gmail.com>
2024-04-09 12:18:13 +0100

    add displaying double deck od shuffled cards

--- src/components/MemoryGame/index.jsx
+++ src/components/MemoryGame/index.jsx
@@ -1,5 +1,32 @@
+const cards = [1, 2, 3, 4, 5];
+function shuffle(array) {
+  console.log(array);
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  console.log(array);
+  return array;
+}
+
+const allCards = shuffle(cards.concat(cards)); // make a doubled deck of
+
+const handleCkick = () => {};
 function Game() {
-  return <div>Memory Game</div>;
+  return (
+    <>
+      <div className="cardsContainer">
+        {allCards.map((item, index) => {
+          return (
+            <div key={index} className="cards" onClick={handleCkick}>
+              <div className="cardBack"></div>
+              {item}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Game;
