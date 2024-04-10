4164732bad9898fd9f08308028c62a6588d3e7fc
Anna Chernova <annanvks@gmail.com>
2024-04-10 14:01:17 +0100

    changed nav and title

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -7,22 +7,15 @@ function MyNavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Pair-Up Puzzle</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+            <Nav.Link href="#home">Rules</Nav.Link>
+            <NavDropdown title="Options" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Set 1</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Set 2</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Set 3</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
